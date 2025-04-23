ab0f92c05ecf287dd2df4c51d66a1b1c98ce341f
Gabriel França de Albuquerque Pernambuco <gfap@cesar.school>
2025-04-23 09:58:20 -0300

    testando

--- meu_app/views.py
+++ meu_app/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Questionario
 from .models import Alimento, Substituicao, Questionario
 from django.http import HttpResponse
 
