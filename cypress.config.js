487515392ec62f5559467bb67e1f1476a101ae7e
Yan Ribeiro Nunes <yrn@cesar.school>
2025-04-30 18:28:42 -0300

    novo teste CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const { exec } = require("child_process");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,8 +7,18 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
     watchForFileChanges: false,
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        resetDb() {
+          return new Promise((resolve, reject) => {
+            exec("rm -f db.sqlite3", (err, stdout, stderr) => {
+              if (err) return reject(err);
+              resolve(stdout || stderr);
+            });
+          });
+        },
+      });
     },
   },
 });
