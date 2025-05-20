f230279a533f63f50e8ca8ca4ab5ac1e0d7657cc
Gabriel França de Albuquerque Pernambuco <gfap@cesar.school>
2025-05-20 14:08:03 -0300

    ajustando arquivo yml

--- meu_projeto/urls.py
+++ meu_projeto/urls.py
@@ -4,11 +4,6 @@ from meu_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('meu_app.urls', 'meu_app'), namespace='meu_app')),
-]
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.redirecionar_para_login),
     path('login/', views.login_view, name='login'),
     path('cadastro/', views.cadastro_view, name='cadastro'),
@@ -17,11 +12,10 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('editar-perfil/', views.editar_perfil, name='editar_perfil'),
     path('excluir-perfil/', views.excluir_perfil, name='excluir_perfil'),
-    path('receitas/', views.receitas_view, name='receitas'),  # <-- esta linha resolve o erro
+    path('receitas/', views.receitas_view, name='receitas'),
     path('agua/', views.agua_view, name='agua'),
     path('cardapio/', views.cardapio_view, name='cardapio'),
     path('dicas-nutricionais/', views.dicas_nutricionais, name='dicas_nutricionais'),
     path('dados/', views.dadoscadastrais, name='dados'),
-    path('admin/', admin.site.urls),
-    path('', include(('meu_app.urls', 'meu_app'), namespace='meu_app')),
+    path('app/', include(('meu_app.urls', 'meu_app'), namespace='meu_app')),  # prefixado para evitar conflito
 ]
